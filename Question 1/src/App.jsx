706e1534213af4b010eda5a1f4b0e43583e3ddf6
Srishti Soni <2006.sirisoni@gmail.com>
2026-05-07 11:59:40 +0530

    Add files via upload

---
+++ Question 1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NameInput from "./components/NameInput";
+
+function App() {
+  return (
+    <div>
+      <h1>React Real-time Input Demo</h1>
+      <NameInput />
+    </div>
+  );
+}
+
+export default App;
